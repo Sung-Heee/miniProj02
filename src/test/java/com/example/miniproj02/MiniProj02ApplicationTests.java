6edecaa16865a14cf4c2f4636eef7264484af790
Sung-Heee <praaa0220@gmail.com>
2024-04-26 13:51:32 +0900

    first commit

---
+++ src/test/java/com/example/miniproj02/MiniProj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproj02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProj02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
